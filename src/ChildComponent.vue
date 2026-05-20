781b7108262f5627ba009eb058b0182cb7b384f6
Tom Kisha <x0rsw1tch@gmail.com>
2026-05-20 14:06:00 -0400

    Initial commit

---
+++ src/ChildComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    This is child component.
+    <h1>{{ text }}</h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'ChildComponent',
+  props: {
+    text: {
+      type: String,
+      required: true,
+    }
+  }
+}
+</script>
